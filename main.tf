824765eb2a1f17c244890a61119bd7c41b45bb9a
vishnureddy <tarigopula34@gmail.com>
2025-08-26 21:42:48 +0530

    terraform roboshop project dev and qa env

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ module "tools" {
   volume_size = each.value["volume_size"]
   instance_type = each.value["instance_type"]
   domain_name = var.domain_name
-  zone_id     = "var.zone_id"
+  zone_id     = var.zone_id
 
 }

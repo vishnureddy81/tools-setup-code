f18d08923b1d0e6bcde81a087a03e14c6b1a8892
vishnureddy <tarigopula34@gmail.com>
2025-08-26 18:36:59 +0530

    terraform roboshop project dev and qa env

---
+++ misc/vault_secrets/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-d81"
+    key    = "vault_secrets/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "vault" {
+  address = "http://vault-internal.vishnureddy.online:8200"
+  #vault private ip address we have to provide it.
+  token   = var.vault_token
+  skip_tls_verify = true
+}
+
+variable "vault_token" {}
+
+resource "vault_generic_secret" "roboshop-dev" {
+  path      = "roboshop/fronted"
+  data_json = <<EOT
+
+{
+"foo": "bar"
+"pizza": "cheese"
+
+}
+EOT
+
+}

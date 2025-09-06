fcafdf12e8e7773a0fe245606c18861a3d5a13ea
vishnureddy <tarigopula34@gmail.com>
2025-09-06 17:35:50 +0530

    terraform roboshop project dev and qa env

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -1,3 +1,44 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-d81"
+#     key    = "vault_secrets/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+#
+# provider "vault" {
+#   address = "http://vault-internal.vishnureddy.online:8200"
+#   #vault private ip address we have to provide it.
+#   token   = var.vault_token
+#   skip_tls_verify = true
+# }
+#
+# variable "vault_token" {}
+#
+# resource "vault_mount" "roboshop-dev" {
+#   path        = "roboshop-dev"
+#   type        = "kv"
+#   options     = { version = "2" }
+#   description = "KV Version 2 secret engine mount"
+# }
+#
+#
+#
+# resource "vault_generic_secret" "roboshop-dev" {
+#   path      = "${vault_mount.roboshop-dev.path}/fronted"
+#
+#   data_json = <<EOT
+# {
+# "foo": "bar"
+# "pizza": "cheese"
+#
+# }
+# EOT
+#
+# }
+
+
+######
 terraform {
   backend "s3" {
     bucket = "terraform-d81"
@@ -7,32 +48,30 @@ terraform {
 }
 
 provider "vault" {
-  address = "http://vault-internal.vishnureddy.online:8200"
-  #vault private ip address we have to provide it.
-  token   = var.vault_token
+  address         = "http://vault-internal.vishnureddy.online:8200"
+  # Vault private IP or DNS we have to provide
+  token           = var.vault_token
   skip_tls_verify = true
 }
 
 variable "vault_token" {}
 
-resource "vault_mount" "roboshop-dev" {
+# Create a new KV v2 secret engine
+resource "vault_mount" "roboshop_dev" {
   path        = "roboshop-dev"
   type        = "kv"
   options     = { version = "2" }
   description = "KV Version 2 secret engine mount"
 }
 
-
-
-resource "vault_generic_secret" "roboshop-dev" {
-  path      = "${vault_mount.roboshop-dev.path}/fronted"
+# Create a secret under roboshop-dev/frontend
+resource "vault_generic_secret" "roboshop_dev_frontend" {
+  path = "${vault_mount.roboshop_dev.path}/frontend"
 
   data_json = <<EOT
 {
-"foo": "bar"
-"pizza": "cheese"
-
+  "foo": "bar",
+  "pizza": "cheese"
 }
 EOT
-
 }

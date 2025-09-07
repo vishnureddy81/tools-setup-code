5d4d90a66d67287f92ab06451e4011129659c5f3
vishnureddy <tarigopula34@gmail.com>
2025-09-07 18:53:32 +0530

    terraform roboshop project dev and qa env

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -1,14 +1,16 @@
 terraform {
   backend "s3" {
     bucket = "terraform-d81"
-    key    = "vault_secrets/terraform.tfstate"
+    key    = "vault-secrets/terraform.tfstate"
     region = "us-east-1"
+
   }
 }
 
 provider "vault" {
-  address = "http://vault-internal.vishnureddy.online:8200"
+  address = "http://vault-internal.rdevopsb81.online:8200"
   token   = var.vault_token
+  skip_tls_verify = true
 }
 
 variable "vault_token" {}
@@ -17,11 +19,11 @@ resource "vault_mount" "roboshop-dev" {
   path        = "roboshop-dev"
   type        = "kv"
   options     = { version = "2" }
-  description = "KV Version 2 secret engine mount"
+  description = "RoboShop Dev Secrets"
 }
 
 resource "vault_generic_secret" "roboshop-dev" {
-  path = "${vault_mount.roboshop-dev.path}/data/frontend"
+  path = "${vault_mount.roboshop-dev.path}/frontend"
 
   data_json = <<EOT
 {

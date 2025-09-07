7a5c0147343901235739d063fcc9e69827d264e9
vishnureddy <tarigopula34@gmail.com>
2025-09-07 18:46:44 +0530

    terraform roboshop project dev and qa env

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -1,3 +1,46 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-d81"
+#     key    = "vault_secrets/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+#
+# provider "vault" {
+#   address = "http://vault-internal.vishnureddy.online:8200"
+#   #vault private ip address we have to provide it.
+#   token   = var.vault_token
+#   skip_tls_verify = true
+# }
+#
+# variable "vault_token" {}
+#
+#
+#
+#
+# resource "vault_mount" "roboshop-dev" {
+#   path        = "roboshop-dev"
+#   type        = "kv"
+#   options     = { version = "2" }
+#   description = "KV Version 2 secret engine mount"
+# }
+#
+#
+#
+# resource "vault_generic_secret" "roboshop-dev" {
+#   path      = "${vault_mount.roboshop-dev.path}/frontend"
+#
+#   data_json = <<EOT
+# {
+#   "foo":   "bar",
+#   "pizza": "cheese"
+# }
+# EOT
+# }
+#
+#
+
+
 terraform {
   backend "s3" {
     bucket = "terraform-d81"
@@ -8,15 +51,10 @@ terraform {
 
 provider "vault" {
   address = "http://vault-internal.vishnureddy.online:8200"
-  #vault private ip address we have to provide it.
   token   = var.vault_token
-  skip_tls_verify = true
 }
 
 variable "vault_token" {}
-
-
-
 
 resource "vault_mount" "roboshop-dev" {
   path        = "roboshop-dev"
@@ -25,10 +63,8 @@ resource "vault_mount" "roboshop-dev" {
   description = "KV Version 2 secret engine mount"
 }
 
-
-
 resource "vault_generic_secret" "roboshop-dev" {
-  path      = "${vault_mount.roboshop-dev.path}/frontend"
+  path = "${vault_mount.roboshop-dev.path}/data/frontend"
 
   data_json = <<EOT
 {
@@ -37,5 +73,3 @@ resource "vault_generic_secret" "roboshop-dev" {
 }
 EOT
 }
-
-

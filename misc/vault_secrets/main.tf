39085cff087bbca8fcc6e0a9944e5bfe1c23afe6
vishnureddy <tarigopula34@gmail.com>
2025-09-08 23:03:01 +0530

    terraform roboshop project dev and qa env

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "vault" {
-  address = "http://vault-internal.vishnureddy.online:8200"
+  address = "http://172.31.17.116:8200"
   token   = var.vault_token
   skip_tls_verify = true
 }
